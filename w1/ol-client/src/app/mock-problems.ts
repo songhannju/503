b121afed05d29b179c8c683b159ae57c991ca6f6
hsong <songhannju@gmail.com>
2018-03-13 22:20:34 -0400

    first week 03132018

---
+++ w1/ol-client/src/app/mock-problems.ts
@@ -0,0 +1,36 @@
+import { Problem } from './models/problem.model';
+
+export const PROBLEMS: Problem[] = [
+{
+  id: 1,
+  name: "Two Sum",
+  desc: 'Given an array of integers, find two numbers such that they add up to a specific target number.',
+  difficulty: "easy"
+},
+{
+  id: 2,
+  name: "3Sum",
+  desc: 'Given an array S of n integers, are there elements a, b, c in S such that a + b + c = 0?',
+  difficulty: "medium"
+},
+{
+  id: 3,
+  name: "4Sum",
+  desc: 'Given an array S of n integers, are there elements a, b, c, and d in S such that a + b + c = 0?',
+  difficulty: "medium"
+},
+{
+  id: 4,
+  name: "Triangle Count",
+  desc: 'Given an array S of n integers, are there elements a, b, c in S such that a + b + c = 0?',
+  difficulty: "hard"
+},
+{
+  id: 5,
+  name: "Sliding WIndow Maximum",
+  desc: 'Given an array S of n integers, are there elements a, b, c, and d in S such that a + b + c = 0?',
+  difficulty: "super"
+},
+
+
+];
